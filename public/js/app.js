5d3cfe7b7b46c54c0904b14d4c95f15bc0ca8ade
Yejiel Wahnich <yejielw@hotmail.com>
2017-04-25 17:14:59 +0300

    first commit for the authentication

--- public/js/app.js
+++ public/js/app.js
@@ -1,48 +1,48 @@
-var app = angular.module('rereddit', ['ui.router']);
+var app = angular.module('homecook', ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
-  $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
 
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: '/templates/home.html',
-      controller: 'PostController'
-    })
-    .state('comment', {
-      url: '/post/:id',
-      templateUrl: '/templates/comments.html',
-      controller: 'CommentController'
-    })
-    .state('login', {
-      url: '/login',
-      templateUrl: '/templates/login.html',
-      controller: 'AuthController'
-    })
-    .state('auth', {
-      url: '/authorization?token&name',
-      controller: function($rootScope, $stateParams, $state, $http) {
-        if ($stateParams.token) {
-          var user = {
-            name: $stateParams.name,
-            token: $stateParams.token
-          }
-          localStorage.setItem("user", JSON.stringify(user));
-          $rootScope.currentUser = user.name;
-          //set the header for all requests
-         $http.defaults.headers.common.Authorization = 'Bearer ' + user.token;
-          $state.go('home');
-        }
-      }
-    })
-  $urlRouterProvider.otherwise('home');
+    //     $stateProvider
+    //         .state('home', {
+    //             url: '/home',
+    //             templateUrl: '/templates/home.html',
+    //             controller: 'PostController'
+    //         })
+    //         .state('comment', {
+    //             url: '/post/:id',
+    //             templateUrl: '/templates/comments.html',
+    //             controller: 'CommentController'
+    //         })
+    //         .state('login', {
+    //             url: '/login',
+    //             templateUrl: '/templates/login.html',
+    //             controller: 'AuthController'
+    //         })
+    //         .state('auth', {
+    //             url: '/authorization?token&name',
+    //             controller: function($rootScope, $stateParams, $state, $http) {
+    //                 if ($stateParams.token) {
+    //                     var user = {
+    //                         name: $stateParams.name,
+    //                         token: $stateParams.token
+    //                     }
+    //                     localStorage.setItem("user", JSON.stringify(user));
+    //                     $rootScope.currentUser = user.name;
+    //                     //set the header for all requests
+    //                     $http.defaults.headers.common.Authorization = 'Bearer ' + user.token;
+    //                     $state.go('home');
+    //                 }
+    //             }
+    //         })
+    //     $urlRouterProvider.otherwise('home');
 
 });
 
 app.run(function($rootScope) {
-  var user = JSON.parse(localStorage.getItem("user"));
-  if (user) {
-    $rootScope.currentUser = user.name;
-  }
+    var user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+        $rootScope.currentUser = user.name;
+    }
 });
--- server.js
+++ server.js
@@ -1,33 +1,144 @@
-//todo
-//everything you need to get a server up and running
-//middleware, routes, database connection etc
-//package and module requirements
-var express = require('express')
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var passport = require('./models/passport');
-var postsRoutes = require('./routes/postsRoutes');
-var authRoutes = require('./routes/authRoutes');
+/**
+ * Module dependencies.
+ */
+const express = require('express');
+const compression = require('compression');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const logger = require('morgan');
+const chalk = require('chalk');
+const errorHandler = require('errorhandler');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('express-flash');
+const path = require('path');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const expressValidator = require('express-validator');
+const expressStatusMonitor = require('express-status-monitor');
+const sass = require('node-sass-middleware');
 
-//let's get going...
-var app = express();
-mongoose.connect(process.env.CONNECTION_STRING||'mongodb://localhost/posts');
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ */
+dotenv.load({ path: '.env.example' });
 
-app.use(passport.initialize());
+/**
+ * Controllers (route handlers).
+ */
+const homeRoute = require('./routes/home');
+const usersRoute = require('./routes/user');
+
+/**
+ * API keys and Passport configuration.
+ */
+const passportConfig = require('./config/passport');
+
+/**
+ * Create Express server.
+ */
+const app = express();
+
+/**
+ * Connect to MongoDB.
+ */
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+    console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+    process.exit();
+});
+
+/**
+ * Express configuration.
+ */
+app.set('port', process.env.PORT || 3000);
+
+app.use(expressStatusMonitor());
+app.use(compression());
+app.use(sass({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public')
+}));
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
-//some middleware that we need
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+    store: new MongoStore({
+        url: process.env.MONGODB_URI || process.env.MONGOLAB_URI,
+        autoReconnect: true,
+        clear_interval: 3600
+    })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use((req, res, next) => {
+    res.locals.user = req.user;
+    next();
+});
+app.use((req, res, next) => {
+    // After successful login, redirect back to the intended page
+    if (!req.user &&
+        req.path !== '/login' &&
+        req.path !== '/signup' &&
+        !req.path.match(/^\/auth/) &&
+        !req.path.match(/\./)) {
+        req.session.returnTo = req.path;
+    } else if (req.user &&
+        req.path == '/account') {
+        req.session.returnTo = req.path;
+    }
+    next();
+});
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 
-app.use('/posts', postsRoutes);
-app.use('/auth', authRoutes);
+/**
+ * Primary app routes.
+ */
+app.get('/', homeRoute.index);
+app.get('/login', usersRoute.getLogin);
+app.post('/login', usersRoute.postLogin);
+app.get('/logout', usersRoute.logout);
+app.get('/forgot', usersRoute.getForgot);
+app.post('/forgot', usersRoute.postForgot);
+app.get('/reset/:token', usersRoute.getReset);
+app.post('/reset/:token', usersRoute.postReset);
+app.get('/signup', usersRoute.getSignup);
+app.post('/signup', usersRoute.postSignup);
+//app.get('/contact', contactController.getContact);
+//app.post('/contact', contactController.postContact);
+app.get('/account', passportConfig.isAuthenticated, usersRoute.getAccount);
+// app.post('/account/profile', passportConfig.isAuthenticated, userController.postUpdateProfile);
+// app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
+// app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
+// app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
 
-app.all('*', function(req, res) {
-  res.sendFile(__dirname + "/public/index.html")
+app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
+    res.redirect(req.session.returnTo || '/');
 });
 
-//start the server
-app.listen(process.env.PORT || '8000');
+/**
+ * Error Handler.
+ */
+app.use(errorHandler());
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get('port'), () => {
+    console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env')); 
+    console.log('  Press CTRL-C to stop\n');
+});
+
+module.exports = app;
